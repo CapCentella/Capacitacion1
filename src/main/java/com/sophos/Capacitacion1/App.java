8f3f313b9570c75f29aa8d36b942934f20fe4af8
CapCentella <carmorion@hotmail.com>
2020-06-16 10:16:41 -0500

    primer commit

---
+++ src/main/java/com/sophos/Capacitacion1/App.java
@@ -0,0 +1,13 @@
+package com.sophos.Capacitacion1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
